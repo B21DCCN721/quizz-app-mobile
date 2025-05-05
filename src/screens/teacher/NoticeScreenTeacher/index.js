7b8c995fbc536215a346ad00fd7728212c8feaea
B21DCCN721 <daoxuantri2003@gmail.com>
2025-05-05 23:36:33 +0700

    xoa folder teacher de merge cua nam

--- src/screens/teacher/NoticeScreenTeacher/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultLayout from "../../../layouts/DefaultLayout";
-
-function NoticeScreenTeacher() {
-    return (  
-        <DefaultLayout/>
-    );
-}
-
-export default NoticeScreenTeacher;
